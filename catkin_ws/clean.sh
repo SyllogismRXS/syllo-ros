5c28bcd9b114434ebd5aa91092746785b5ec7370
Kevin DeMarco <kevin.demarco@gtri.gatech.edu>
2013-08-13 01:15:32 -0400

    initial add of serial

--- catkin_ws/clean.sh
+++ catkin_ws/clean.sh
@@ -5,5 +5,5 @@ catkin_make clean
 rm -rf build
 rm -rf devel
 
-rm -rf ./src/videoray/gui/build*
+rm -rf ./src/videoray/gui/*build*
 rm -rf ./src/videoray/gui/videoray/*.user
---
+++ catkin_ws/src/videoray/src/comm/Packetizer.cpp
@@ -0,0 +1,2 @@
+#include "Packetizer.h"
+
